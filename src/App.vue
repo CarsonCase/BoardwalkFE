fc5ac911ccf84037e03923e885354ca807252cb6
Charles Kozel <shadowck5000@gmail.com>
2022-05-12 17:45:13 -0700

    Add placeholder swaps page

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container-lg">
-            <a class="navbar-brand h1 mb-0" href="#">Boardwalk</a>
+            <RouterLink class="navbar-brand h1 mb-0" to="/" active-class="active">Boardwalk</RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarContent">
                 <div class="navbar-nav">
-                    <RouterLink class="nav-link" to="/" active-class="active">Home</RouterLink>
                     <RouterLink class="nav-link" to="/about" active-class="active">About</RouterLink>
+                    <RouterLink class="nav-link" to="/swaps" active-class="active">Swaps</RouterLink>
                 </div>
             </div>
         </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
             // which is lazy-loaded when the route is visited.
             component: () => import("../views/AboutView.vue"),
         },
+        {
+            path: "/swaps",
+            name: "swaps",
+            component: () => import("../views/SwapsView.vue"),
+        },
     ],
 });
 
