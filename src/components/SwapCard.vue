9bce391491eb425a5ba245640a185d6457b9ffb0
Charles Kozel <shadowck5000@gmail.com>
2022-05-14 12:31:10 -0700

    Add basic swaps card

---
+++ src/components/SwapCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Strategy } from "@/model/Strategy";
+
+defineProps({
+    strategy: Strategy,
+});
+</script>
+
+<template>
+    <div class="card bg-secondary">
+        <div class="card-body">
+            <h5 class="card-title">{{ strategy.name }}</h5>
+            <ul>
+                <li>Balance: {{ strategy.balance }}</li>
+                <li>TVL: {{ strategy.tvl }}</li>
+                <li>Approved?: {{ strategy.isApproved }}</li>
+                <li>Streaming?: {{ strategy.isStreaming }}</li>
+            </ul>
+            <button class="btn btn-primary">
+                Buy Swap
+            </button>
+        </div>
+    </div>
+</template>
--- src/views/SwapsView.vue
+++ src/views/SwapsView.vue
@@ -2,6 +2,7 @@
 import WalletConnectionInit from "@/components/WalletConnectionInit.vue";
 import { ref } from "vue";
 import { Strategy } from "@/model/Strategy";
+import SwapCard from "@/components/SwapCard.vue";
 
 const strategies = ref([
     new Strategy("ETH Holding", "0x4e706CbbE94B2867BA35A989fbe9859bEC045759")
@@ -12,5 +13,12 @@ const strategies = ref([
     <div class="container-lg">
         <h1>This is the Swaps view</h1>
         <WalletConnectionInit />
+    </div>
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-4">
+                <SwapCard :strategy="strategies[0]" />
+            </div>
+        </div>
     </div>
 </template>
